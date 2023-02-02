bb102d1184c12c8badfd393c276662713b8e8cea
Marilena-C <marilena.cancel@gmail.com>
2023-02-02 00:00:36 +0000

    Arranged the links in html file, as the java script file was not placed correctly before

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,71 @@
-// Use Moment.js to format the date and assign to the declared variable.
-// Declare the time display variables for calendar day
-var timeDisplayEl = $("#currentDay");
-var timeBlocksEl = $("#calendar");
+
+// Use Moment.js to format the date and assign to the declared variable
+// Declare the time display variables for calendar working day
+var dayDisplayEl = $("#currentDay");
+var timeBlocksEl = $(".container");
 var timeHour = [
   "9AM",
   "10AM",
   "11AM",
-  "12AM",
+  "12PM",
   "1PM",
   "2PM",
   "3PM",
   "4PM",
   "5PM",
 ];
-// Check the moment in time andle displaying the time
+// Check the actual moment in time and format time display
 function displayTime() {
   var rightNow = moment().format("dddd, MMMM Do");
-  timeDisplayEl.text(rightNow);
+  dayDisplayEl.text(rightNow);
 }
+
+//Define variable for the actual time, that will be compared ten  with the hour in each div on calendar day. Declare variable for text input from user which is stored 
 var compActTime = moment().hour();
 var storedText = [];
-function readSavedText() {
+function readStoredText() {
   for (var i = 0; i < timeHour.length; i++) {
     if (JSON.parse(localStorage.getItem(timeHour[i])) == null) {
       storedText[i] = "";
-    } else {
+    } 
+    else {
       storedText[i] = JSON.parse(localStorage.getItem(timeHour[i]));
     }
   }
 }
-readSavedText();
-for (var i = 0; i < timeHour.length; i++) {
-  var listEl = $("<div>").addClass("row time-block");
-  var timeEl = $("<div>")
-    .addClass("hour col-lg-2 col-md-2 col-sm-3")
+readStoredText();
+
+  for (var i = 0; i < timeHour.length; i++) {
+   var list = $("<div>").addClass("row time-block");
+   var time = $("<div>")
+    .addClass("hour col-lg-2 col-md-2 col-sm-12")
     .text(timeHour[i]);
-  var textEl = $("<textarea>")
-    .addClass("description col-lg-9 col-md-9 col-sm-7")
+   var text = $("<textarea>")
+    .addClass("description col-lg-9 col-md-9 col-sm-12")
     .attr("id", timeHour[i])
     .text(storedText[i]);
-    //Compare current time with working hours startinwith 9am
+  //Format the hour' divs depending on the moment in time - compare current hour with working hours, starting with 9am  (0 + 9)
   if (compActTime < [i + 9]) {
-    textEl.addClass("future");
+    text.addClass("future");
   } else if (compActTime > [i + 9]) {
-    textEl.addClass("past");
+    text.addClass("past");
   } else {
-    textEl.addClass("present");
+    text.addClass("present");
   }
-  var saveEl = $("<button>")
-    .addClass("saveBtn col-lg-1 col-md-1 col-sm-2")
+//Button to save test when user clicks on it
+  var save = $("<button>")
+    .addClass("saveBtn col-lg-1 col-md-1 col-sm-12")
     .attr("data-letter", timeHour[i])
-    .text("Save");
-  listEl.append(timeEl);
-  listEl.append(textEl);
-  listEl.append(saveEl);
-  timeBlocksEl.append(listEl);
-}
+    .text("save");
+  list.append(time);
+  list.append(text);
+  list.append(save);
+  timeBlocksEl.append(list);
+  }
+
 $(".saveBtn").on("click", function () {
-  var Id = $(this).attr("data-letter");
-  var textId = "#" + Id;
-  var text = $(textId).val();
-  //   console.log(text);
-  localStorage.setItem(Id, JSON.stringify(text));
+  var hour = $(this).attr("data-letter");
+    var text = $("#"+hour).val();
+  localStorage.setItem(hour, JSON.stringify(text));
 });
 setInterval(displayTime, 1000);
-
-
-
-// TODO: 6. Parse the following Unix timestamp, 1318781876, and convert into any time/date format.
-var unixFormat;
-$("#6a").text(unixFormat);
-
